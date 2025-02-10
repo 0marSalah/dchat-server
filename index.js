6148e8e184a1414ac08f9527f4c1358a2bfef2f6
Omar Salah <omar.salah1597@gmail.com>
2025-02-11 00:47:05 +0200

    implement encryption and decryption functionality using AES-256-GCM

--- index.js
+++ index.js
@@ -1,17 +1,42 @@
+const Gun = require('gun');
 const express = require('express');
 const cors = require('cors');
 
+const crypto = require('crypto');
+
+const secretKey = crypto.randomBytes(32).toString('hex');
+
+function encrypt(text, secretKey) {
+  const iv = crypto.randomBytes(12); // Initialization Vector
+  const cipher = crypto.createCipheriv(
+    'aes-256-gcm',
+    Buffer.from(secretKey, 'hex'),
+    iv
+  );
+
+  let encrypted = cipher.update(text, 'utf8', 'hex');
+  encrypted += cipher.final('hex');
+
+  const authTag = cipher.getAuthTag().toString('hex');
+  return iv.toString('hex') + ':' + encrypted + ':' + authTag;
+}
+
 const app = express();
-const port = 3000;
-
 app.use(cors());
+app.use(express.json());
 
-
-app.get('/', (req, res) => {
-    console.log(req.url);
-    res.json({message:'Hello, world!'});
+app.route('/').get((req, res) => {
+  res.send('GunDB server running');
 });
 
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+app.route('/encrypt').post((req, res) => {
+  const { message } = req.body;
+  const encryptedRSA = encrypt(message, secretKey);
+  res.json({ encryptedRSA });
 });
+
+const server = app.listen(3000, () => {
+  console.log('GunDB server running on http://localhost:3000');
+});
+
+const gun = Gun({ web: server });
