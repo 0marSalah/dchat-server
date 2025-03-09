0e4c39f1b401d60bcbf7577a96d373cddc6beb66
kholoudxs55kh <kfattem@gmail.com>
2025-03-09 04:45:12 +0200

    add password hashing and JWT token creation utilities; update TypeScript configuration and types

---
+++ src/utils/hashing_pass.ts
@@ -0,0 +1,15 @@
+import bcrypt from 'bcrypt'
+
+export const hashingPassword = async (password: string): Promise<string> => {
+    return bcrypt.hashSync(password, +(process.env.HASH_SECRET as string))
+}
+
+export const isPassword = async (
+    password: string | undefined,
+    hashedPassword: string | undefined | null
+): Promise<boolean> => {
+    if (!password || !hashedPassword) {
+        return false
+    }
+    return bcrypt.compareSync(password, hashedPassword)
+}
---
+++ types.d.ts
@@ -0,0 +1,53 @@
+declare global {
+  enum AuthType {
+    GMAIL = "gmail",
+    REGULAR = "regular",
+  }
+
+  interface Data {
+    type: AuthType;
+    name: string;
+    email: string;
+    password?: string;
+    image?: string;
+    isRemember?: boolean;
+  }
+
+  interface User {
+    id: string;
+    name: string;
+    email: string;
+    password: string | null | undefined;
+    image: string | null | undefined;
+    refreshToken: string | null | undefined;
+    accessToken: string | null | undefined;
+    createdAt: Date;
+    updatedAt: Date;
+  }
+
+  interface UserRegister {
+    name: string;
+    email: string;
+    password: string;
+    image?: string;
+  }
+
+  interface UserRegisterGmail {
+    name: string;
+    email: string;
+    password?: string;
+    image?: string;
+  }
+
+  interface UserRequest {
+    id: string;
+    email: string;
+  }
+
+  interface UserLogin {
+    email: string;
+    password: string;
+  }
+}
+
+export {}
